e7acc3386f39bd2d232a43c0a8664f3cd3a1df5d
Rajashekar <Rajashekar@DESKTOP-K97MTSM>
2022-08-03 18:27:10 +0530

    updated the code

---
+++ Client/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SWA_Client.Data;
+
+[assembly: HostingStartup(typeof(SWA_Client.Areas.Identity.IdentityHostingStartup))]
+namespace SWA_Client.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
